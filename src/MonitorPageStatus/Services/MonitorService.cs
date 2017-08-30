eee6458db5cc36051d482648779f2d4c4dbf7efd
Mattias Andersson <mattiasandersson88@hotmail.com>
2017-08-30 17:06:14 +0200

    Added MonitorService and MonitorConfiguration

---
+++ src/MonitorPageStatus/Services/MonitorService.cs
@@ -0,0 +1,25 @@
+﻿using MonitorPageStatus.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonitorPageStatus.Services
+{
+    public class MonitorService : IMonitorService, IDisposable
+    {
+        IEmailService _emailService;
+        IHttpService _httpService;
+
+        public MonitorService(MonitorConfiguration monitorConfiguration, EmailConfiguration emailConfiguration, HttpConfiguration httpConfiguration)
+        {
+            _emailService = new EmailService(emailConfiguration);
+            _httpService = new HttpService(httpConfiguration);
+        }
+
+        public void Dispose()
+        {
+            _httpService.Dispose();
+            _emailService.Dispose();
+        }
+    }
+}
