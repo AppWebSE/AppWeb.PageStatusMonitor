649b8f13f00eaa1d152a841ff51692a1a1b8de85
Mattias Andersson <mattias.t.andersson@gmail.com>
2018-10-27 08:25:31 +0200

    Moved example actions to ConsoleActions class (#7)

---
+++ MonitorPageStatus/Actions/ConsoleActions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using MonitorPageStatus.Models;
+
+namespace MonitorPageStatus.Actions
+{
+    public static class ConsoleActions
+    {
+        public static Action<MonitorResult> WriteSuccessful = (monitorResult) => {
+            Console.WriteLine();
+            Console.WriteLine("Successful checks:");
+            foreach (var result in monitorResult.Results.Where(x => x.Successful))
+            {
+                Console.WriteLine($"{result.MonitorItem.ToString()} - ({result.Milliseconds}ms)");
+            }
+        };
+
+        public static Action<MonitorResult> WriteFailed = (monitorResult) => {
+            Console.WriteLine();
+            Console.WriteLine("Not successful checks:");
+            foreach (var result in monitorResult.Results.Where(x => !x.Successful))
+            {
+                Console.WriteLine($"{result.MonitorItem.ToString()} - ({result.Milliseconds}ms)");
+            }
+        };
+    }
+}
