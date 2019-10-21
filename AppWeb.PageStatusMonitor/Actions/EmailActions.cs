30d91b8f3d03878f773741c22e850582ca4005f1
Mattias Andersson <mattias.t.andersson@gmail.com>
2019-10-21 13:29:57 +0200

    Some asynchronous improvements to prevent potential deadlocks in UI applications

--- AppWeb.PageStatusMonitor/Actions/EmailActions.cs
+++ AppWeb.PageStatusMonitor/Actions/EmailActions.cs
@@ -13,7 +13,7 @@ namespace AppWeb.PageStatusMonitor.Actions
                 string subject = $"Check for '{monitorResultItem.MonitorItem}' failed";
                 string body = $"<html><body><h1>Checked failed</h1><p>Check: {monitorResultItem.MonitorItem}</p><p>Successful: {monitorResultItem.Successful}</p><p>Execution time: {monitorResultItem.Milliseconds}</p></body></html>";
                 
-                emailService.SendEmail(subject, body, true);
+                emailService.SendEmailAsync(subject, body, true).GetAwaiter().GetResult();
             }
         };
     }
